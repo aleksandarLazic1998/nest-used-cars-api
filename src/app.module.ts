bfc369b13946153e055035ff6d40e8d1c2b421be
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2023-07-31 20:29:48 +0200

    Create controllers, modules, services for users and repositories

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { RepositoriesModule } from './repositories/repositories.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, RepositoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
