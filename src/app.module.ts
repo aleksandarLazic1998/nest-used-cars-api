142e04531d0584bf9845e66d56834383e45aae72
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2023-08-02 16:48:30 +0200

    Set global configuration for typeorm.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import DBConfigModule from './config/dbConfig';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [DBConfigModule, UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
