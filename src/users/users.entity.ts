d5690cf5291cb69ac7fb9a598749257b8b860527
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2023-08-02 17:01:11 +0200

    Create user entity.

---
+++ src/users/users.entity.ts
@@ -0,0 +1,11 @@
+import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  email: string;
+
+  password: string;
+}
